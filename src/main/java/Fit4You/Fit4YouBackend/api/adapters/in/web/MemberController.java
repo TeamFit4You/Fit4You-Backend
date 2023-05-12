64e2209c412a0b8c7dc33dec23eb9d41edcd7856
JJBINY <97151887+JJBINY@users.noreply.github.com>
2023-05-12 23:17:24 +0900

    Revert "Infra/actions-build  (#10)" (#12)
    This reverts commit 82896046cbe738034a75f64c19040d528efe34eb.

--- src/main/java/Fit4You/Fit4YouBackend/api/adapters/in/web/MemberController.java
+++ src/main/java/Fit4You/Fit4YouBackend/api/adapters/in/web/MemberController.java
@@ -8,9 +8,6 @@ import Fit4You.Fit4YouBackend.api.dto.request.SignUpRequest;
 import Fit4You.Fit4YouBackend.api.dto.response.SessionResponse;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,22 +22,18 @@ import java.util.Date;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@Tag(name = "Member", description = "Member API Document")
 public class MemberController {
 
     private final MemberUseCase memberUseCase;
     private final AppConfig appConfig;
 
     @PostMapping("/members/sign-up")
-    @Operation(summary = "회원가입 요청", description = "회원가입")
-    @ApiResponse(responseCode = "409",description = "Conflicted")
     public void signUp(@RequestBody @Valid SignUpRequest request){
         memberUseCase.signUp(request);
     }
 
 
     @PostMapping("/members/sign-in")
-    @Operation(summary = "로그인", description = "로그인이요")
     public SessionResponse signIn(@RequestBody @Valid SignInRequest request){
 
         log.info("로그인 요청 - {}", request.getEmail());
@@ -54,12 +47,12 @@ public class MemberController {
                 .setExpiration(new Date(iat.getTime()+1000*60*60))//만료시간 - ms단위;1000=1초
                 .signWith(secretKey)
                 .compact();
-        return new SessionResponse(memberId,jws);
+        return new SessionResponse(jws);
     }
 
-//    @Auth
-//    @GetMapping("/members/auth/test")
-//    public void test(){
-//        log.info(">>> 인증 테스트 요청 성공");
-//    }
+    @Auth
+    @GetMapping("/members/auth/test")
+    public void test(){
+        log.info(">>> 인증 테스트 요청 성공");
+    }
 }
--- src/main/java/Fit4You/Fit4YouBackend/api/dto/request/TrainingCreate.java
+++ src/main/java/Fit4You/Fit4YouBackend/api/dto/request/TrainingCreate.java
@@ -4,10 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-<<<<<<< HEAD
-
-=======
->>>>>>> 00b6878d02ccec6a17d98a3ea189e9f8accc85dd
 import java.util.List;
 
 @Getter
@@ -23,9 +19,6 @@ public class TrainingCreate {
     public TrainingCreate(String email, List<Long> selects) {
         this.email = email;
         this.selects = selects;
-<<<<<<< HEAD
-=======
 
->>>>>>> 00b6878d02ccec6a17d98a3ea189e9f8accc85dd
     }
 }
