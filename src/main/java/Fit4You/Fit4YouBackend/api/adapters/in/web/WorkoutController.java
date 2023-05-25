3109921f1ba2194167ff71f80a80b89d55bc5b43
JJBINY <97151887+JJBINY@users.noreply.github.com>
2023-05-25 21:30:45 +0900

    Add HttpMethod API Docs (#33)
    * [Add] feedback generate
    
    * [Docs] http method 설명추가

--- src/main/java/Fit4You/Fit4YouBackend/api/adapters/in/web/WorkoutController.java
+++ src/main/java/Fit4You/Fit4YouBackend/api/adapters/in/web/WorkoutController.java
@@ -2,6 +2,7 @@ package Fit4You.Fit4YouBackend.api.adapters.in.web;
 
 import Fit4You.Fit4YouBackend.api.application.ports.in.WorkoutUseCase;
 import Fit4You.Fit4YouBackend.api.dto.response.EstimationResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class WorkoutController {
 
 
     @PostMapping(value = "/workouts/{workoutId}/estimation/{setNo}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "정확도 평가 요청", description = "운동 수행 영상의 정확도 평가 요청")
     public EstimationResponse estimation(@RequestParam("file")MultipartFile file, @PathVariable Long workoutId, @PathVariable Integer setNo) {
         // 파일이 비어있는지 확인
 //        if (file.isEmpty()) {
