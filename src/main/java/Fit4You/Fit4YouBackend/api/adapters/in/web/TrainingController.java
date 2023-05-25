4a959bf678d54a29b66f18ca0a1fb948fa04744d
JJBINY <sisoya0424@gmail.com>
2023-05-25 21:29:01 +0900

    [Docs] http method 설명추가

--- src/main/java/Fit4You/Fit4YouBackend/api/adapters/in/web/TrainingController.java
+++ src/main/java/Fit4You/Fit4YouBackend/api/adapters/in/web/TrainingController.java
@@ -5,6 +5,7 @@ import Fit4You.Fit4YouBackend.config.interceptors.Auth;
 import Fit4You.Fit4YouBackend.api.application.ports.in.TrainingUseCase;
 import Fit4You.Fit4YouBackend.api.dto.request.RecommendCreate;
 import Fit4You.Fit4YouBackend.api.dto.response.TrainingResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,12 +23,14 @@ public class TrainingController {
 
     @Auth
     @GetMapping("/trainings/recommend")
+    @Operation(summary = "추천 운동 리스트 요청", description = "사용자의 현재 상태를 바탕으로 추천 운동 리스트(3개) 요청")
     public TrainingResponse startRecommend(@RequestBody @Valid RecommendCreate request){
         return trainingUseCase.createRecommend(request);
     }
 
     @Auth
     @GetMapping("/trainings/create")
+    @Operation(summary = "운동 리스트 요청", description = "사용자가 선택한 운동(id)들로 운동리스트를 구성")
     public TrainingResponse startTraining(@RequestBody @Valid TrainingCreate request){
         return trainingUseCase.createTraining(request);
     }
