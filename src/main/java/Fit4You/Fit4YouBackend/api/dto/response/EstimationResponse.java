52751040b0008d44cc79f48ed131430b377fe0bd
JJBINY <sisoya0424@gmail.com>
2023-05-25 20:43:38 +0900

    [Add] feedback generate

--- src/main/java/Fit4You/Fit4YouBackend/api/dto/response/EstimationResponse.java
+++ src/main/java/Fit4You/Fit4YouBackend/api/dto/response/EstimationResponse.java
@@ -3,17 +3,21 @@ package Fit4You.Fit4YouBackend.api.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class EstimationResponse {
 
     private Float accuracy;
-    private String feedback;
-
+    private String feedback = "";
+    private List<String> feedbackParts;
     @Builder
-    public EstimationResponse(Float accuracy, String feedback) {
+    public EstimationResponse(Float accuracy, List<String> feedbackParts) {
         this.accuracy = accuracy;
-        this.feedback = feedback;
+        this.feedbackParts = feedbackParts;
     }
 }
